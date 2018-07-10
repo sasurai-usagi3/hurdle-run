f927badbf92e1d0431252730220b46ccac04b8e6
宇山 拓夢 <sasurai.usagi3@gmail.com>
2018-07-11 07:28:03 +0900

    fix: ファイル名修正

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCharacter : MonoBehaviour {
+public class Character : MonoBehaviour {
 	bool isJumping;
 	Rigidbody2D rigidbody;
 
